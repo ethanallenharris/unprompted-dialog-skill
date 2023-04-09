5ad6ccdc982549368a6d266d5e3dcb7f735d87b0
Ethan Allen-Harris <ethanaharris10@gmail.com>
2023-04-09 21:07:34 +0100

    added dialog, randomly will speak to user (not tested)

--- __init__.py
+++ __init__.py
@@ -1,15 +1,28 @@
 from mycroft import MycroftSkill, intent_handler
+import random
 
+
+
+
+MINUTES = 1 #seconds
 
 class UnpromptedDialog(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_handler('dialog.unprompted.intent')
-    def handle_dialog_unprompted(self, message):
-        self.speak_dialog('dialog.unprompted')
-
-
-def create_skill():
-    return UnpromptedDialog()
-
+        self.listen_event = None
+        
+    def initialize(self):  
+        # Creates repeating event to talk unpromted  
+        self.schedule_repeating_event(self.speak, datetime.now(), 60 * MINUTES, name='unprompted')
+        # If frequency list doesn't exist yet instantiate it
+        if 'frequency' not in self.settings:
+            self.settings['frequency'] = [10]
+            
+    def speak(self):
+        # Calculate the chance of speaking based on the stored frequency number
+        frequency = self.settings['frequency'][0]
+        if random.randint(0, 10) <= frequency:
+            # Speak a random dialog
+            self.speak_dialog(random.choice(['unprompted.generic', 'unpromted.didyouknow']))
+            # Listen for a response
+            response = self.get_response('prompt')
