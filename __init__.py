0f92a10d3af0facdff23273c71ec594a4ceed05a
Ethan Allen-Harris <ethanaharris10@gmail.com>
2023-04-10 12:40:42 +0100

    push for testing

--- __init__.py
+++ __init__.py
@@ -9,20 +9,22 @@ MINUTES = 1 #seconds
 class UnpromptedDialog(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.listen_event = None
         
     def initialize(self):  
         # Creates repeating event to talk unpromted  
         self.schedule_repeating_event(self.speak, datetime.now(), 60 * MINUTES, name='unprompted')
         # If frequency list doesn't exist yet instantiate it
         if 'frequency' not in self.settings:
-            self.settings['frequency'] = [10]
+            self.settings['frequency'] = []
             
     def speak(self):
+        self.speak_dialog('unprompted.generic')
         # Calculate the chance of speaking based on the stored frequency number
-        frequency = self.settings['frequency'][0]
-        if random.randint(0, 10) <= frequency:
+        #frequency = self.settings['frequency'][0]
+        if random.randint(0, 10) <= 10:
             # Speak a random dialog
-            self.speak_dialog(random.choice(['unprompted.generic', 'unpromted.didyouknow']))
-            # Listen for a response
-            response = self.get_response('prompt')
+            response = self.get_response(random.choice(['unprompted.generic', 'unpromted.didyouknow']))
+            
+    @intent_handler('change.frequency.intent')
+    def handle_frequency_change(self, message):
+        self.speak_dialog('unprompted.generic')
